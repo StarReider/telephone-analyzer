bf9c5521046a839c699c4b4778dd49068ec10af4
starreider15@gmail.com <starreider15@gmail.com@bdaf6d8b-8ae9-2c15-e22f-375b486456b8>
2014-02-02 17:42:40 +0000

    (Resolves issue #22) : Maven support

--- trunk/src/main/java/org/robe/ta/TelephoneAnalyzer.java
+++ trunk/src/main/java/org/robe/ta/TelephoneAnalyzer.java
@@ -11,67 +11,69 @@ import org.robe.ta.data.DataFactory;
 import org.robe.ta.data.DataProvider;
 import org.robe.ta.ui.MainFrame;
 
-public class TelephoneAnalyzer 
+public class TelephoneAnalyzer
 {
-        private final Log log;
-        
-        public TelephoneAnalyzer() throws Exception
-        {
-                log = LogFactory.getLog(TelephoneAnalyzer.class); 
-                try 
-                {
-                        init();
-                } 
-                catch (Exception e) 
-                {
-                        JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                        log.error(e);
-                        throw e;
-                }
-        }
+    private final Log log;
 
-        private void init() throws Exception 
+    public TelephoneAnalyzer() throws Exception
+    {
+        log = LogFactory.getLog(TelephoneAnalyzer.class);
+        try
         {
-                log.info("Configuring starting...");
-                ConfigurationReader configurationReader = new ConfigurationReader();
-                log.info("Configuring done");    
-                
+            init();
+        } 
+        catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            log.error("Error", e);
+            throw e;
+        }
+    }
+
+    private void init() throws Exception
+    {
+        log.info("Configuring starting...");
+        ConfigurationReader configurationReader = new ConfigurationReader();
+        log.info("Configuring done");
+
         log.info("TelephoneAnalyzer v." + configurationReader.getVersion());
-        
-        log.info("Initialize db subsystem starting...");                
-        DataFactory dataFactory = DataFactory.getInstance();
-        DataProvider dataFacade = dataFactory.getDataFacade(configurationReader.getMode(), configurationReader.getJDBCURL());
-        log.info("Initialize db subsystem done");       
-        
-        log.info("Initialize GUI subsystem starting...");
-                createGUI(dataFacade, configurationReader);
-        log.info("Initialize GUI subsystem done");
-        }
-        
-        private void createGUI(final DataProvider dataFacade, final ConfigurationReader configurationReader) throws Exception 
-        {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                
-                SwingUtilities.invokeLater(new Runnable() 
-                {
-                        @Override
-                        public void run() 
-                        {
-                                try
-                                {
-                                        new MainFrame(dataFacade, configurationReader);
-                                } 
-                                catch (Exception e) 
-                                {
-                                        log.error(e);
-                                        JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                                }
-                        }
-                });
-        }
 
-        public static void main(String[] args) throws Exception 
+        log.info("Initialize db subsystem starting...");
+        DataFactory dataFactory = DataFactory.getInstance();
+        DataProvider dataFacade = dataFactory.getDataFacade(configurationReader.getMode(),
+                configurationReader.getJDBCURL());
+        log.info("Initialize db subsystem done");
+
+        log.info("Initialize GUI subsystem starting...");
+        createGUI(dataFacade, configurationReader);
+        log.info("Initialize GUI subsystem done");
+    }
+
+    private void createGUI(final DataProvider dataFacade, final ConfigurationReader configurationReader)
+            throws Exception
+    {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        SwingUtilities.invokeLater(new Runnable()
         {
-                new TelephoneAnalyzer();
-        }
+            @Override
+            public void run()
+            {
+                try
+                {
+                    new MainFrame(dataFacade, configurationReader);
+                } 
+                catch (Exception e)
+                {
+                    log.error("Error", e);
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        new TelephoneAnalyzer();
+    }
 }
